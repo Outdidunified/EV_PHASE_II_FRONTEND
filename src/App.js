3f84b8f4c2eec0b550e6619b2d02a86208f10eb4
KesavanOutdid <kesavan@outdidtech.com>
2024-07-01 12:21:31 +0530

    associationadmin upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SuperAdminApp from './adminApps/SuperAdminApp';
 import ResellerAdminApp from './adminApps/ResellerAdminApp';
+import AssociationAdminApp from './adminApps/AssociationAdminApp';
 
 const App = () => {
   
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/superadmin/*" element={<SuperAdminApp />} />
         <Route path="/reselleradmin/*" element={<ResellerAdminApp />} />
+        <Route path="/associationadmin/*" element={<AssociationAdminApp />} />
         <Route path="/" element={<Navigate to="/superadmin" />} />
       </Routes>
     </Router>
