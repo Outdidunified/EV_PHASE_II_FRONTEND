e575f0d098bd7c6f87aee75c2903c4ec1217f389
KesavanOutdid <kesavan@outdidtech.com>
2024-07-19 17:49:52 +0530

    super and association admin logout issue fix

--- src/adminApps/SuperAdminApp.js
+++ src/adminApps/SuperAdminApp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import Login from '../superadmin/page/Login';
 import Dashboard from '../superadmin/page/Dashboard';
 import ManageDevice from '../superadmin/page/ManageDevices/ManageDevice';
@@ -19,16 +19,20 @@ import ManageUsers from '../superadmin/page/ManageUsers/ManageUsers';
 import ViewUserList from '../superadmin/page/ManageUsers/ViewUserList';
 import EditUserList from '../superadmin/page/ManageUsers/EditUserList';
 import Profile from '../superadmin/page/Profile';
+import Header from '../superadmin/components/Header';
 
 const SuperAdminApp = () => {
   const storedUser = JSON.parse(sessionStorage.getItem('superAdminUser'));
   const [loggedIn, setLoggedIn] = useState(!!storedUser);
   const [userInfo, setUserInfo] = useState(storedUser || {});
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (storedUser) {
       setLoggedIn(true);
       setUserInfo(storedUser);
+    }else {
+      setLoggedIn(false);
     }
   }, [storedUser]);
 
@@ -36,165 +40,170 @@ const SuperAdminApp = () => {
     setUserInfo(data);
     setLoggedIn(true);
     sessionStorage.setItem('superAdminUser', JSON.stringify(data));
+    navigate('/superadmin/Dashboard');
   };
 
   const handleLogout = () => {
     setLoggedIn(false);
     setUserInfo({});
     sessionStorage.removeItem('superAdminUser');
+    navigate('/superadmin');
   };
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={loggedIn ? <Navigate to="/superadmin/Dashboard" /> : <Login handleLogin={handleLogin} />}
-      />
-      <Route
-        path="Dashboard"
-        element={loggedIn ? (
-          <Dashboard userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ManageDevice"
-        element={loggedIn ? (
-          <ManageDevice userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-       <Route
-        path="AddManageDevice"
-        element={loggedIn ? (
-          <AddManageDevice userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ViewManageDevice"
-        element={loggedIn ? (
-          <ViewManageDevice userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="EditManageDevice"
-        element={loggedIn ? (
-          <EditManageDevice userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="AssignReseller"
-        element={loggedIn ? (
-          <AssignReseller userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-       <Route
-        path="ManageReseller"
-        element={loggedIn ? (
-          <ManageReseller userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="AddManageReseller"
-        element={loggedIn ? (
-          <AddManageReseller userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ViewManageReseller"
-        element={loggedIn ? (
-          <ViewManageReseller userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="EditManageReseller"
-        element={loggedIn ? (
-          <EditManageReseller userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="AssignClient"
-        element={loggedIn ? (
-          <AssignClient userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="AssignCharger"
-        element={loggedIn ? (
-          <AssignCharger userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-       <Route
-        path="SessignHistory"
-        element={loggedIn ? (
-          <SessignHistory userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ManageUsers"
-        element={loggedIn ? (
-          <ManageUsers userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ViewUserList"
-        element={loggedIn ? (
-          <ViewUserList userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="EditUserList"
-        element={loggedIn ? (
-          <EditUserList userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="ManageUserRole"
-        element={loggedIn ? (
-          <ManageUserRole userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-      <Route
-        path="Profile"
-        element={loggedIn ? (
-          <Profile userInfo={userInfo} handleLogout={handleLogout} />
-        ) : (
-          <Navigate to="/superadmin" />
-        )}
-      />
-    </Routes>
+    <>
+      {loggedIn && <Header userInfo={userInfo} handleLogout={handleLogout} />}
+      <Routes>
+        <Route
+          path="/"
+          element={loggedIn ? <Navigate to="/superadmin/Dashboard" /> : <Login handleLogin={handleLogin} />}
+        />
+        <Route
+          path="Dashboard"
+          element={loggedIn ? (
+            <Dashboard userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ManageDevice"
+          element={loggedIn ? (
+            <ManageDevice userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="AddManageDevice"
+          element={loggedIn ? (
+            <AddManageDevice userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ViewManageDevice"
+          element={loggedIn ? (
+            <ViewManageDevice userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="EditManageDevice"
+          element={loggedIn ? (
+            <EditManageDevice userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="AssignReseller"
+          element={loggedIn ? (
+            <AssignReseller userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ManageReseller"
+          element={loggedIn ? (
+            <ManageReseller userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="AddManageReseller"
+          element={loggedIn ? (
+            <AddManageReseller userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ViewManageReseller"
+          element={loggedIn ? (
+            <ViewManageReseller userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="EditManageReseller"
+          element={loggedIn ? (
+            <EditManageReseller userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="AssignClient"
+          element={loggedIn ? (
+            <AssignClient userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="AssignCharger"
+          element={loggedIn ? (
+            <AssignCharger userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="SessignHistory"
+          element={loggedIn ? (
+            <SessignHistory userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ManageUsers"
+          element={loggedIn ? (
+            <ManageUsers userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ViewUserList"
+          element={loggedIn ? (
+            <ViewUserList userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="EditUserList"
+          element={loggedIn ? (
+            <EditUserList userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="ManageUserRole"
+          element={loggedIn ? (
+            <ManageUserRole userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+        <Route
+          path="Profile"
+          element={loggedIn ? (
+            <Profile userInfo={userInfo} handleLogout={handleLogout} />
+          ) : (
+            <Navigate to="/superadmin" />
+          )}
+        />
+      </Routes>
+    </>
   );
 };
 
